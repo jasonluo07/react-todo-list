706f4082933b75c2ca5947880a44aeca86a591e0
jasonluo <yulaie1012@gmail.com>
2022-12-05 22:36:40 +0800

    Formate code

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,18 +1,18 @@
 export default function Todo({ name, isCompleted, id }) {
   return (
-    <li className='todo stack-small'>
-      <div className='c-cb'>
-        <input id='todo-0' type='checkbox' defaultChecked={isCompleted} />
-        <label className='todo-label' htmlFor={id}>
+    <li className="todo stack-small">
+      <div className="c-cb">
+        <input id="todo-0" type="checkbox" defaultChecked={isCompleted} />
+        <label className="todo-label" htmlFor={id}>
           {name}
         </label>
       </div>
-      <div className='btn-group'>
-        <button type='button' className='btn'>
-          Edit <span className='visually-hidden'>{name}</span>
+      <div className="btn-group">
+        <button type="button" className="btn">
+          Edit <span className="visually-hidden">{name}</span>
         </button>
-        <button type='button' className='btn btn__danger'>
-          Delete <span className='visually-hidden'>{name}</span>
+        <button type="button" className="btn btn__danger">
+          Delete <span className="visually-hidden">{name}</span>
         </button>
       </div>
     </li>
