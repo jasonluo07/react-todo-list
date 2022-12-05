6ba390a01ebd4cbc98d3e21171a7cf174e6198dd
jasonluo <yulaie1012@gmail.com>
2022-12-05 23:55:40 +0800

    Count tasks

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,9 @@ export default function App(props) {
     setTasks([...tasks, newTask]);
   }
 
+  const tasksNoun  = taskList.length !== 1 ? "tasks" : "task";
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
   return (
     <div className="todoapp stack-large">
       <h1>React Todo List</h1>
@@ -32,9 +35,7 @@ export default function App(props) {
         <FilterButton />
         <FilterButton />
       </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-      </h2>
+      <h2 id="list-heading">{headingText}</h2>
       <ul
         role="list"
         className="todo-list stack-large stack-exception"
