9d641d6fa27458631543b8755c2f461daad39025
jasonluo <yulaie1012@gmail.com>
2022-12-06 01:16:04 +0800

    hotfix: <Todo /> defaultChecked

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -56,7 +56,7 @@ export default function Todo(props) {
         <input
           id={props.id}
           type="checkbox"
-          defaultChecked={props.completed}
+          defaultChecked={props.isCompleted}
           onChange={() => props.toggleTaskCompleted(props.id)}
         />
         <label className="todo-label" htmlFor={props.id}>
