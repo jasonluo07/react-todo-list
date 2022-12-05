cab4e85f5d46fd21655ac612c01efe2144322490
jasonluo <yulaie1012@gmail.com>
2022-12-06 00:00:14 +0800

    style: change props usage as parameter

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,20 +1,18 @@
 export default function Todo(props) {
-  const { id, name, isCompleted } = props;
-
   return (
     <li className="todo stack-small">
       <div className="c-cb">
-        <input id="todo-0" type="checkbox" defaultChecked={isCompleted} />
-        <label className="todo-label" htmlFor={id}>
-          {name}
+        <input id="todo-0" type="checkbox" defaultChecked={props.isCompleted} />
+        <label className="todo-label" htmlFor={props.id}>
+          {props.name}
         </label>
       </div>
       <div className="btn-group">
         <button type="button" className="btn">
-          Edit <span className="visually-hidden">{name}</span>
+          Edit <span className="visually-hidden">{props.name}</span>
         </button>
         <button type="button" className="btn btn__danger">
-          Delete <span className="visually-hidden">{name}</span>
+          Delete <span className="visually-hidden">{props.name}</span>
         </button>
       </div>
     </li>
